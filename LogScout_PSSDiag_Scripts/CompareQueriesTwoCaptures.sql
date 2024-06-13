3e045fabeaf839a231f40d155fde117df3088fce
1collin <151403673+1collin@users.noreply.github.com>
2024-06-13 18:28:12 -0400

    Update CompareQueriesTwoCaptures.sql

--- LogScout_PSSDiag_Scripts/CompareQueriesTwoCaptures.sql
+++ LogScout_PSSDiag_Scripts/CompareQueriesTwoCaptures.sql
@@ -50,3 +50,71 @@ BEGIN
 		FULL OUTER JOIN Baseline b ON s.HashID = b.HashID
 	ORDER BY [ΔAvgDuration_Millisec] DESC
 END
+
+
+
+------------
+
+
+--Compare waits for specific HashIDs
+DECLARE @UTC_Offset INT = -7;
+
+WITH Snaps AS
+(
+	SELECT 
+		  MAX(r.runtime) [Lastsnap]
+		, b.HashID
+		, r.session_id
+		, r.wait_type
+		, MAX(r.wait_duration_ms) [wait_ms]
+	FROM ReadTrace.tblBatches b
+		JOIN dbo.tbl_REQUESTS r ON b.Session = r.session_id
+			AND r.runtime <= DATEADD(HOUR, @UTC_Offset, b.EndTime)
+			AND r.runtime >= DATEADD(HOUR, @UTC_Offset, b.StartTime)
+	WHERE HashID IN (-7758464401401233043, 3262107551579857174, -618862393263134308, -9048367324748729896, -6355130179974594400)
+		AND wait_type NOT IN ('CXCONSUMER', 'WAITFOR')
+	GROUP BY 
+		  b.HashID
+		, r.session_id
+		, r.request_start_time
+		, r.wait_type
+), BaselineSnaps AS
+(
+	SELECT 
+		  MAX(r.runtime) [Lastsnap]
+		, b.HashID
+		, r.session_id
+		, r.wait_type
+		, MAX(r.wait_duration_ms) [wait_ms]
+	FROM [PerfScenario1_Baseline].ReadTrace.tblBatches b
+		JOIN [PerfScenario1_Baseline].dbo.tbl_REQUESTS r ON b.Session = r.session_id
+			AND r.runtime <= DATEADD(HOUR, @UTC_Offset, b.EndTime)
+			AND r.runtime >= DATEADD(HOUR, @UTC_Offset, b.StartTime)
+	WHERE HashID IN (-7758464401401233043, 3262107551579857174, -618862393263134308, -9048367324748729896, -6355130179974594400)
+		AND wait_type NOT IN ('CXCONSUMER', 'WAITFOR')
+	GROUP BY 
+		  b.HashID
+		, r.session_id
+		, r.request_start_time
+		, r.wait_type
+), Summary AS
+(
+	SELECT s.HashID, s.wait_type, SUM(s.wait_ms) [wait_ms]
+	FROM Snaps s
+	GROUP BY s.HashID, s.wait_type
+), BaselineSummary AS
+(
+	SELECT s.HashID, s.wait_type, SUM(s.wait_ms) [wait_ms]
+	FROM BaselineSnaps s
+	GROUP BY s.HashID, s.wait_type
+)
+SELECT 
+	  COALESCE(s.HashID, b.HashID) [HashID]
+	, COALESCE(s.wait_type, b.wait_type) [wait_type]
+	, ISNULL(s.wait_ms, 0)-ISNULL(b.wait_ms, 0) [ΔSampledWaitDurationMs]
+	, s.wait_ms [SlowerWaitDurationMs]
+	, b.wait_ms [BLWaitDurationMs]
+FROM Summary s
+	FULL OUTER JOIN BaselineSummary b ON s.HashID = b.HashID
+		AND s.wait_type = b.wait_type
+ORDER BY [ΔSampledWaitDurationMs] DESC
